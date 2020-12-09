ceca23f51f459dff6a9622096ec6125961271f54
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-09 02:20:58 +0000

    Create Product and Category db models, add db models to admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Category, Product
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+
+    )
+
+    oredring = ('id')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
