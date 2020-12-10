a494a506730d71839873a808b3cd71f53f023188
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-10 05:15:41 +0000

    Add product page and view to render it

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import products_page
+from .views import products_page, product_detail
 
 urlpatterns = [
     path('', products_page, name='products'),
+    path('<product_id>', product_detail, name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -54,3 +54,13 @@ def products_page(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
