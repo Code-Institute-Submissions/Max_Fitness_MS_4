4a1e3bd25d25576576d8bd282c63c0b0e8136123
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-11 06:26:01 +0000

    Update shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,14 +1,26 @@
+from products.models import Product
+from django.shortcuts import get_object_or_404
 
 
 def bag_contents(request):
 
-    grand_total = 0
-    bag_items = 0
+    total = 0
     product_count = 0
+    bag_items = []
+    bag = request.session.get('bag', {})
+
+    for item_id, name in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price
+        product_count += 1
+        bag_items.append({
+            'item_id': item_id,
+            'product': product,
+        })
 
     context = {
         "bag_items": bag_items,
-        "grand_total": grand_total,
+        "total": total,
         "product_count": product_count,
     }
 
