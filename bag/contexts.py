e7d7090e43e0031b8796d4fddc1346f1e31dd2f1
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-12 03:28:03 +0000

    Update bag functionality logic to handle subscriptions and proucts

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,5 @@
 from products.models import Product
+from subscriptions.models import Membership
 from django.shortcuts import get_object_or_404
 
 
@@ -8,14 +9,26 @@ def bag_contents(request):
     product_count = 0
     bag_items = []
     bag = request.session.get('bag', {})
+    subscription_bag = request.session.get('subscription_bag', {})
 
-    for item_id, name in bag.items():
+    for item_id, category in bag.items():
         product = get_object_or_404(Product, pk=item_id)
         total += product.price
         product_count += 1
         bag_items.append({
             'item_id': item_id,
+            'category': category,
             'product': product,
+        })
+
+    for item_id, category in subscription_bag.items():
+        subscription = get_object_or_404(Membership, pk=item_id)
+        total += subscription.price
+        product_count += 1
+        bag_items.append({
+            'item_id': item_id,
+            'category': category,
+            'subscription': subscription,
         })
 
     context = {
--- bag/views.py
+++ bag/views.py
@@ -12,17 +12,28 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add item to bag """
 
-    itemName = request.POST.get('name')
+    category = request.POST.get('category')
+    subscriptionType = None
+    if 'subscriptionType' in request.POST:
+        subscriptionType = request.POST.get('subscriptionType')
     redirect_url = request.POST.get('redirect_url')
 
     bag = request.session.get('bag', {})
+    subscription_bag = request.session.get('subscription_bag', {})
 
-    if item_id in list(bag.keys()):
-        messages.warning(request, f"""Item {itemName} has already
-         been added to your bag.""")
+    if subscriptionType:
+        if item_id in list(subscription_bag.keys()):
+            messages.warning(request, """Item has already been
+            added to your bag.""")
+        else:
+            subscription_bag[item_id] = category
+            request.session['subscription_bag'] = subscription_bag
     else:
-        bag[item_id] = itemName
-
-    request.session['bag'] = bag
+        if item_id in list(bag.keys()):
+            messages.warning(request, """Item has already been
+            added to your bag.""")
+        else:
+            bag[item_id] = category
+            request.session['bag'] = bag
 
     return redirect(redirect_url)
