97b064c4dfebbdc375740c522b8d2cfb383804c9
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-20 14:12:23 +0000

    Updated webhook handler to handle profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,6 +4,7 @@ from .models import Order, OrderLineItem
 from products.models import Product
 
 from subscriptions.models import Membership
+from profiles.models import UserProfile
 
 import json
 import time
@@ -35,7 +36,6 @@ class StripeWH_Handler:
         bag = json.loads(intent.metadata.bag)
         subscription_bag = json.loads(intent.metadata.subscription_bag)
         save_info = intent.metadata.save_info
-        user = intent.metadata.user
 
         # Unpack json and append it to array
         bag_items = []
@@ -56,6 +56,23 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.user
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_email = billing_details.email
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_postcode = shipping_details.address.postal_code
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_county = shipping_details.address.state
+                profile.save()
 
         order_exists = False
         attempt = 1
@@ -91,6 +108,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
