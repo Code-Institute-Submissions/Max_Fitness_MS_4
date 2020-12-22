ce895d008e606f555be085a4df9921fc99ba3a19
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-22 15:56:15 +0000

    Add sorting logic to blog page

--- blog/views.py
+++ blog/views.py
@@ -8,10 +8,26 @@ def render_blog(request):
     Renders blog page for the user
     """
     allPosts = BlogPost.objects.all()
-    print(allPosts)
+    sort = None
+    direction = None
+    current_sorting = None
 
+    if request.GET:
+        if 'sort' in request.GET:
+            sortKey = request.GET['sort']
+            sort = sortKey
+            if sortKey == 'date':
+                sortKey = 'date_added'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortKey = f'-{sortKey}'
+            allPosts = allPosts.order_by(sortKey)
+
+    current_sorting = f'{sort}_{direction}'
     template = "blog/blogs.html"
     context = {
         'blogPosts': allPosts,
+        'current_sorting': current_sorting,
     }
     return render(request, template, context)
