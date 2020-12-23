5efe44d0578cc561507c468a69e3e4dd6d2fb740
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-23 23:47:10 +0000

    Update home app view logic and add blog component to index page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-
+from blog.models import BlogPost
 # Create your views here.
 
 
 def home_page(request):
-    return render(request, 'home/index.html')
+    blogPost = BlogPost.objects.order_by('-date_added')[:3]
+
+    context = {
+        'blogPost': blogPost,
+    }
+    return render(request, 'home/index.html', context)
