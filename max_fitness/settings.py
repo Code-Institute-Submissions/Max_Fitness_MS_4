5ade63fc8daa6dbe14a2dafc37cd1aca2eac59f7
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-24 13:49:09 +0000

    Add Procfile and Setup for pre-deployment

--- max_fitness/settings.py
+++ max_fitness/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 from pathlib import Path
 
@@ -27,7 +28,7 @@ SECRET_KEY = 'gx^e@fqmz02$l1d59+jn8vyw7_l778cdtwyy+a&0+)9$&v9xmt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['max-fitness-milestone-4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -128,12 +129,18 @@ WSGI_APPLICATION = 'max_fitness.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 
 # Password validation
