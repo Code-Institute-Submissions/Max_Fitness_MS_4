78e0d7d45098cb1cea2a35f03ddef61f32cdbb86
Manojlovic1998 <Manojlovic911@hotmail.com>
2020-12-24 14:02:20 +0000

    Remove secret key and set debug to none

--- max_fitness/settings.py
+++ max_fitness/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gx^e@fqmz02$l1d59+jn8vyw7_l778cdtwyy+a&0+)9$&v9xmt'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['max-fitness-milestone-4.herokuapp.com', 'localhost']
 
